e23c2ae05b28876521838df9f5badcd931f2ca62
Asher <asherjohn48@gmail.com>
2025-01-16 19:54:11 +0800

    actual server

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 const dbURI = process.env.dbURI;
 
-// mongoose
-//   .connect(dbURI)
-//   .then(() => {
-//     console.log('Connected to database');
-//     app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
-//   })
-//   .catch(err => {
-//     console.log('Could not connect to database');
-//     console.error(err);
-//   });
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    console.log('Connected to database');
+    app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+  })
+  .catch(err => {
+    console.log('Could not connect to database');
+    console.error(err);
+  });
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 // middleware logger
 app.use((req, res, next) => {
@@ -36,11 +36,11 @@ app.use((req, res, next) => {
 
 
 app.get('/', (req, res) => {
-  res.render('index.html', { title: 'Home', greet: 'Home page' });
+  res.render('index', { title: 'Home', greet: 'Home page' });
 });
 
 // routes
-// app.use('/blogs', blogRoutes);
+app.use('/blogs', blogRoutes);
 
 app.use((req, res) => {
   res.status(404).render('404', { title: '404', message: 'Ooops, page not found!' });
@@ -50,7 +50,5 @@ app.use((err, req, res) => {
   console.error(err);
   res.status(500).send('Something went wrong.');
 });
-
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
 
 module.exports = app;
